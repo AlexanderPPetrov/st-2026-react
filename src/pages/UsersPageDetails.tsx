81119a4f553598513aa6b1aa42afff88386f176b
Aleksander <apetrov@uni-plovdiv.bg>
2026-05-16 11:04:40 +0300

    useParams example

--- src/pages/UsersPageDetails.tsx
+++ src/pages/UsersPageDetails.tsx
@@ -1,9 +1,13 @@
 
+import { useParams } from "react-router"
+
+
 function UsersPageDetails () {
-    
+    const { id } = useParams()
+    //TODO you can now fetch data for a specific user from his ID
     return (
         <>
-            User Details
+            User Details {id}
         </>
     )
 }
